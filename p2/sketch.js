a88b868b4184833d1baff7445dcd7d43055e078b
SplatboyDev <64037464+splatboydev@users.noreply.github.com>
2026-01-27 20:11:31 -0800

    Refactor note drawing logic in sketch.js

--- p2/sketch.js
+++ p2/sketch.js
@@ -12,6 +12,16 @@ let barY, barHeight;
 
 let circleRadius;
 
+let time = 0;
+let speed;
+
+let spawnDelay = 0.1;
+
+let notes = {
+  0: 1,
+  1: 0.3
+}
+
 function setup() {
   createCanvas(400, 400);
   
@@ -19,14 +29,20 @@ function setup() {
   barHeight = height / 6;
   
   circleRadius = height / 6 - 10;
+  
+  speed = 100;
 }
 
 function draw() {
   background(220);
   
   drawBar();
-  drawNote();
+  drawNotes();
   drawTarget();
+  
+  text("X: " + mouseX + ", Y:" + mouseY, mouseX, mouseY);
+  
+  time += deltaTime;
 }
 
 function drawBar() {
@@ -40,6 +56,33 @@ function drawTarget() {
   fill(255)
 }
 
-function drawNote() {
-  circle(width / 2, barY, circleRadius);
+/*
+  Note start at (width + circleRadius)
+  Finish at (circleRadius - 20) at time = notes[i]
+  Going in between should involve some speed.
+*/
+
+//... after around 30 mins of working on this I realized there's a Prototype (sorta like OOP) but i don't know how to use it. Will figure out another time.
+function drawNotes() {
+  for (i=0; i<(Object.keys(notes).length); i++) {
+    let timeTillApproach = max(notes[i] - time/1000, 0);
+    
+    if (notes[i] - spawnDelay <= time) {
+      
+    }
+    //console.log(timeTillApproach);
+    
+    if ((time/1000 < (notes[i]))) {
+      //console.log(""+i+" not on screen")
+      circle(circleRadius - 20, barY, circleRadius);
+    } else {
+      //circle(width + (timeTillApproach / notes[i] * width ), barY, circleRadius);
+    }
+    
+    circle(noteX(timeTillApproach, i), barY, circleRadius);
+  }
+}
+
+function noteX(timeTillApproach, i) {
+  return width - (timeTillApproach / notes[i] * width);
 }
